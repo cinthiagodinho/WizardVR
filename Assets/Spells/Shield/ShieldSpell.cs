14395fac2b1c49ee01efa2a21f0a74b81cc595c4
cinthiag <cinthiago0@gmail.com>
2019-01-10 10:28:40 +0100

    pop cube fonction for Eleonore

--- Assets/Spells/Shield/ShieldSpell.cs
+++ Assets/Spells/Shield/ShieldSpell.cs
@@ -6,21 +6,48 @@ public class ShieldSpell : MonoBehaviour
 {
     Rigidbody rb;
     public float timeTillDeath;
+    public int durability;
+    private float timer;
+    private bool successfulParade = false;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();     
+        rb = GetComponent<Rigidbody>();
         StartCoroutine(DestroySelf());
     }
 
     void FixedUpdate()
     {
+        timer += Time.deltaTime;
+
+        if (durability <= 0)
+            Destroy(gameObject);
 
     }
 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<FirePower>())
+        {
+            if (timer <= 2)
+            {
+                float speed = col.gameObject.GetComponent<FirePower>().speed;
+                Vector3 force = new Vector3(col.gameObject.transform.position.x, col.gameObject.transform.position.y, speed);
+                col.gameObject.GetComponent<Rigidbody>().AddRelativeForce(-force, ForceMode.Impulse);
+                successfulParade = true;
+            }
+            else
+            {
+                durability--;
+            }
+        }
+    }
     IEnumerator DestroySelf()
     {
         yield return new WaitForSeconds(timeTillDeath);
         Destroy(gameObject);
+    }
+    public bool getSuccessfulParade(){
+        return successfulParade;
     }
 }
